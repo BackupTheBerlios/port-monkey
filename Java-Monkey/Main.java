046136ab8dbd8f5d06253e1d47eb09ee454c0afc
giuly <giuly>
2004-01-28 12:50:31 +0000

    NewLine Fix

--- Java-Monkey/Main.java
+++ Java-Monkey/Main.java
@@ -24,87 +24,87 @@ import java.io.*;
 
 class Main
 {
+  static FileOutputStream lofp = null;
+
   public static void main(String[] argv)
   {
     try {
-      System.out.println("Opening Logfile...");
+      System.out.print("Opening Logfile...");
       String logname = new String("pubs");
-      FileOutputStream lofp = new FileOutputStream(logname);
+      lofp = new FileOutputStream(logname);
     } catch(Exception e) {
-      System.out.println("ERROR\n");
+      System.out.println("ERROR");
       System.exit(1);
     }
-    System.out.println("OK\n");
+    System.out.println("OK");
 				    
-  
-    HashSet hosts = new HashSet();
-    System.out.println("Pub-Monkey 0.00 Alpha started\n");
+    System.out.println("Pub-Monkey 0.00 Alpha started");
   
     String host = new String();
   
     String fistr, sestr, thstr, fostr;
 
-    if(argv[1] == "")
+    if(argv[0] == "")
     {
-      System.out.println("Usage: " + argv[0] + " <Hostname or IP>\n");
-      hosts.add("localhost");
+      System.out.println("Usage: " + argv[0] + " <Hostname or IP>");
+      testIt("localhost");
     } else {
-      host = argv[1];
+      host = argv[0];
       int dot;
       if(host.indexOf("*") != -1)
       {      
         dot = host.indexOf(".");
-        fistr = host.substring(0, dot);
-        host = host.substring(dot+1, host.length()-dot);
-      
+	fistr = host.substring(0, dot);
+	host = host.substring(dot+1);
+	
         dot = host.indexOf(".");
-        sestr = host.substring(0, dot);
-        host =  host.substring(dot+1, host.length()-dot);
-    
+	sestr = host.substring(0, dot);
+        host =  host.substring(dot+1);
+	
+
         dot = host.indexOf(".");
-        thstr = host.substring(0, dot);
-        host =  host.substring(dot+1, host.length()-dot);
-    
+	thstr = host.substring(0, dot);
+        host =  host.substring(dot+1);
+	
+
         dot = host.indexOf(".");
-        fostr = host.substring(0, dot);
-        host =  host.substring(dot+1, host.length()-dot);
+        
+	fostr = host;
      
         if(fistr.equals("*"))
         {
           for(int i=1; i != 256; i++)
   	  {
-	    hosts.add(Integer.toString(i) + "." +  sestr + "." + thstr + "." + fostr);
+	    testIt(Integer.toString(i) + "." +  sestr + "." + thstr + "." + fostr);
 	  }
         } else if(sestr.equals("*")) {
           for(int i=1; i != 256; i++)
 	  {
-	    hosts.add(fistr + "." +  Integer.toString(i) + "." + thstr + "." + fostr);
+	    testIt(fistr + "." +  Integer.toString(i) + "." + thstr + "." + fostr);
 	  }
         } else if(thstr.equals("*")) {
           for(int i=1; i != 256; i++)
 	  {
-	    hosts.add(fistr + "." +  sestr + "." + Integer.toString(i) + "." + fostr);
+	    testIt(fistr + "." +  sestr + "." + Integer.toString(i) + "." + fostr);
 	  }
         } else if(fostr.equals("*")) {
           for(int i=1; i != 256; i++)
           {
-            hosts.add(fistr + "." +  sestr + "." + thstr + "." + Integer.toString(i));
+            testIt(fistr + "." +  sestr + "." + thstr + "." + Integer.toString(i));
           }
         } else {
-          hosts.add(host);
+          testIt(host);
         }
       } else {
-        hosts.add(host);
+        testIt(host);
       }
     }
+  }
   
-    Iterator it = hosts.iterator();
-    FtpTest affe;
-    while(it.hasNext())
-    {
-      System.out.println("\n");
-      affe = new FtpTest((String)it.next(), lofp);
-      affe.work();
-    }
+  static public void testIt(String host)
+  {
+     System.out.println("");
+     FtpTest affe = new FtpTest(host, lofp);
+     affe.work();
   }
 }
